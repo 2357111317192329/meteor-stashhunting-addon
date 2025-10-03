3207fd193690f48a7b356809ecce93b8a9d0bddc
miles <mtdaly@ncsu.edu>
2025-10-02 20:57:47 -0400

    Fixed NoJumpDelay crash

--- src/main/java/com/stash/hunt/mixin/LivingEntityMixin.java
+++ src/main/java/com/stash/hunt/mixin/LivingEntityMixin.java
@@ -1,13 +1,14 @@
 package com.stash.hunt.mixin;
 
 import com.stash.hunt.modules.ElytraFlyPlusPlus;
-import meteordevelopment.meteorclient.systems.modules.Module;
+import com.stash.hunt.modules.NoJumpDelay;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,13 +29,15 @@ public abstract class LivingEntityMixin extends Entity
     @Shadow
     public abstract Brain<?> getBrain();
 
-    Module noJumpDelay = Modules.get().get(com.stash.hunt.modules.NoJumpDelay.class);
+    @Unique
+    NoJumpDelay noJumpDelay = Modules.get().get(NoJumpDelay.class);
+    @Unique
     ElytraFlyPlusPlus efly = Modules.get().get(ElytraFlyPlusPlus.class);
 
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/entity/LivingEntity;tickMovement()V")
+    @Inject(at = @At("HEAD"), method = "tickMovement()V")
     private void tickMovement(CallbackInfo ci)
     {
-        if (mc.player != null && mc.player.getBrain().equals(this.getBrain()) && efly != null && efly.enabled() || noJumpDelay.isActive())
+        if (mc.player != null && mc.player.getBrain().equals(this.getBrain()) && (efly != null && efly.enabled()) || (noJumpDelay != null && noJumpDelay.isActive()))
         {
             this.jumpingCooldown = 0;
         }
