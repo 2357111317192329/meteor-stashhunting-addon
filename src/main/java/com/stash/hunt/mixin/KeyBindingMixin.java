d1593311a5fd4fb2fe803d572459d06bf1c7cbb0
2357111317192329 <h811163@nehs.hc.edu.tw>
2026-07-10 02:23:28 +0800

    update to 26.1.2

--- src/main/java/com/stash/hunt/mixin/KeyBindingMixin.java
+++ src/main/java/com/stash/hunt/mixin/KeyBindingMixin.java
@@ -2,7 +2,7 @@ package com.stash.hunt.mixin;
 
 import com.stash.hunt.modules.ElytraFlyPlusPlus;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,20 +11,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(KeyBinding.class)
+@Mixin(KeyMapping.class)
 public abstract class KeyBindingMixin {
 
     @Final
     @Shadow
-    private String id;
+    private String name;
 
-    @Inject(at = @At("RETURN"), method = "isPressed", cancellable = true)
+    @Inject(at = @At("RETURN"), method = "isDown", cancellable = true)
     public void isPressed(CallbackInfoReturnable<Boolean> cir)
     {
         if (Modules.get() != null)
         {
             ElytraFlyPlusPlus efly = Modules.get().get(ElytraFlyPlusPlus.class);
-            if (efly != null && efly.isActive() && efly.enabled() && id.equals("key.forward"))
+            if (efly != null && efly.isActive() && efly.enabled() && name.equals("key.forward"))
             {
                 cir.setReturnValue(true);
             }
